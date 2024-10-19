096f67845bf37330c23bb29c1745692f77b6d883
hibah <hibahraza@outlook.com>
2024-10-19 18:38:00 -0400

    updating code.

--- client.py
+++ client.py
@@ -11,15 +11,33 @@
 """
 # Imports
 import socket
-import threading
+import json
+import os
 import sys
 
 # Configuring Server
 SERVER_HOST = 'localhost'
 SERVER_PORT = 3000
-stopThread = threading.Event()
 
 """
 This is a thread function that 
 will receive messages from the server
 """
+def startingClient():
+    try:
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.connect((SERVER_HOST, SERVER_PORT))
+        data = clientSocket.recv(1024).decode()
+        if data == "Server is full. Please try again later.":
+            print(data)
+            clientSocket.close()
+            return
+        else:
+            clientName = data
+            print(f"Connected to the server as {clientName}")
+        while True:
+            msg = input("Enter message ('exit' to quit): ")
+            if msg:
+                clientSocket.send(msg.encode())
+                if msg.lower() == 'exit':
+                    break
